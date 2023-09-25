c730ec2985489fe4e92676523648be2403c67532
ElishaWhite <123849710+Eli460@users.noreply.github.com>
2023-09-25 00:39:19 -0400

    Finished testing

--- tests/js-basic-02.test.js
+++ tests/js-basic-02.test.js
@@ -37,7 +37,7 @@ TODO: the string literal 'JavaScript is Awesome!'.
 */
 
 // * Add your code BELOW here
-
+let myClass = 'JavaScript is Awesome!';
 // * Don't mess with the test code below here!
 
 test('myClass', () => {
@@ -58,7 +58,7 @@ TODO: Remember to use single-quotes for the empty string.
 */
 
 // * Add your code BELOW here
-
+let emptyString = '0';
 // * Don't mess with the test code below here!
 
 test('emptyString', () => {
@@ -82,7 +82,7 @@ TODO: string with exactly 6 alphanumeric characters in it.
 */
 
 // * Add your code BELOW here
-
+let petName = 'Davies';
 // * Don't mess with the test code below here!
 
 test('petName', () => {
@@ -104,7 +104,7 @@ TODO: assign it any word that starts with a capital A.
 */
 
 // * Add your code BELOW here
-
+const firstLetterIsA = 'Albino';
 // * Don't mess with the test code below here!
 
 test('firstLetterIsA', () => {
@@ -130,7 +130,7 @@ TODO: and two is not a space but an escaped horizontal tab character.
 */
 
 // * Add your code BELOW here
-
+let counting = 'one\two';
 // * Don't mess with the test code below here!
 
 test('counting', () => {
@@ -154,7 +154,7 @@ TODO: assigned variable petName (from Skill 3).
 */
 
 // * Add your code BELOW here
-
+let mySentence = 'Hello, ' + petName;
 // * Don't mess with the test code below here!
 
 test('mySentence', () => {
@@ -182,7 +182,7 @@ TODO: for a JavaScript expression inside a template literal.
 */
 
 // * Add your code BELOW here
-
+let helloPet = `${'Hello, ' + petName};
 // * Don't mess with the test code below here!
 
 test('helloPet', () => {
@@ -211,7 +211,8 @@ TODO: In line 2: Reassign thisMonth to the string literal 'September'.
 */
 
 // * Add your code BELOW here
-
+let thisMonth = 'I dunno!';
+thisMonth = 'September';
 // * Don't mess with the test code below here!
 
 test('thisMonth', () => {
@@ -229,7 +230,7 @@ Template literals are a very important part of modern JavaScript
 and are used to create strings.
 
 When writing code for a string, you should always ask yourself,
-can I use the ${} syntax to insert a JavaScript expression?
+can I use the '${} syntax to insert a JavaScript expression?
 
 https://blog.webdevsimplified.com/2020-03/tagged-template-literals/
 
@@ -245,7 +246,10 @@ TODO: In the below area, you will practice making template literals
 // TODO: You must use the template literal syntax
 
 // * Add your code BELOW here
-
+let year = 2001;
+let firstName = 'John';
+let lastName = 'Smith';
+let message = 'Hi, my name is ' + firstName + lastName + ' and I was born in ' + year;
 // * Don't mess with the test code below here!
 
 test('creating a message', () => {
@@ -264,7 +268,9 @@ test('creating a message', () => {
 // ${price + tax}
 
 // * Add your code BELOW here
-
+let tax = 8.25
+let price = 100;
+let total = 'Your total including tax is $' + (price + tax);
 // * Don't mess with the test code below here!
 
 test('creating a total', () => {
@@ -279,6 +285,9 @@ test('creating a total', () => {
 
 // * Add your code BELOW here
 
+let heading1 = 'JavaScript';
+let heading2 = 'HTML'
+let html = '<h1>JavaScript</h1><h2>HTML</h2><p>This is a paragraph</p>'
 // * Don't mess with the test code below here!
 
 test('html', () => {
